2f6e679a37caf7b04ab3e50ab956fce4df7bc830
Your Name <you@example.com>
2023-05-12 13:24:38 +0300

    duyguu

--- client/src/views/index.ts
+++ client/src/views/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeView } from './HomeView'
 export { default as MarketView } from './MarketView'
 export { default as ProfilePageView } from './ProfilePageView'
+export { default as Wallet } from './Wallet'
